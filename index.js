6c969f3b365cd13d2dd07d88927b2e4fefb24f14
tungbeo4576 <cherrera43nam1311@outlook.com>
2024-05-15 06:58:20 +0000

    feat

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-function isPrime(n) {
-  if (n <= 1) return false;
-  if (n <= 3) return true;
-  if (n % 2 === 0 || n % 3 === 0) return false;
-  let i = 5;
-  while (i * i <= n) {
-    if (n % i === 0 || n % (i + 2) === 0) return false;
-    i += 6;
-  }
-  return true;
-}
+console.log("hello world");
